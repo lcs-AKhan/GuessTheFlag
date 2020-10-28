55fd1dce0cc2e33070f04d651674d9084af3ed3e
Abdul Khan <akhan@lcs.on.ca>
2020-10-28 17:24:20 -0400

    Started working on the GuessTheFlag app. Created a view where there is a text asking for a country and you have to tap on the right country flag from 3 different flags.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            Color.orange.edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of:")
+                    Text(countries[correctAnswer])
+                }
+                .foregroundColor(.white)
+                ForEach( 0 ..< 3 ) { number in
+                    Button(action: {
+                        // Button was tapped
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+
+                    }
+                }
+                Spacer()
+            }
+        }
     }
 }
 
