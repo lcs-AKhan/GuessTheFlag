3df75f4315bc6161cbf5b196c9b76feede904589
Abdul Khan <akhan@lcs.on.ca>
2020-10-28 17:17:02 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Abdul Ahad Khan on 2020-10-28.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
