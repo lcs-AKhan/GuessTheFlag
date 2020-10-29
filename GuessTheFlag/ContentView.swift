df5c6ce728fd61e9f32f4b96376ba1aefc077b9d
Abdul Khan <akhan@lcs.on.ca>
2020-10-29 09:47:12 -0400

    Completed the GuessTheFlag app. Added scoring system and turned flags into buttons. Made it look better.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,32 +9,87 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    // Variables
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var playerScore = 0
     
     var body: some View {
         
         ZStack {
-            Color.orange.edgesIgnoringSafeArea(.all)
+            // Background color
+            LinearGradient(gradient: Gradient(colors: [.blue, .white, .blue]), startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
             
             VStack(spacing: 30) {
                 VStack {
+                    
+                    // Title
                     Text("Tap the flag of:")
+                        .foregroundColor(.white)
+                    
+                    // Flag you need to tap
                     Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                    
+
                 }
-                .foregroundColor(.white)
+                
+                // Flag buttons
                 ForEach( 0 ..< 3 ) { number in
                     Button(action: {
-                        // Button was tapped
+                        self.flagTapped(number)
                     }) {
                         Image(self.countries[number])
                             .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
 
                     }
                 }
+                
+                // Score display
+                Text("Score: \(playerScore)")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .font(.headline)
+                    .padding(.top)
+                
                 Spacer()
             }
         }
+        
+        // Alert that shows up after tapping a flag
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is \(playerScore)"), dismissButton:
+                    .default(Text("Continue")) {
+                        self.askQuestion()
+                    })
+        }
+    }
+    
+    // Scoring system
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            playerScore += 1
+            scoreTitle = "Correct"
+        } else {
+            playerScore = 0
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+    
+    // Shuffle countries
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0 ... 2)
     }
 }
 
